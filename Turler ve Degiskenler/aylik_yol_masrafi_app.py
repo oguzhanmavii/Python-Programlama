c14051866cb61271086ef2bdaa3e8514bd08c627
OĞUZHAN MAVİ <77650437+oguzhanmavii@users.noreply.github.com>
2022-07-27 03:48:54 +0300

    Python-Programlama
    Python ile Sıfırdan İleri Seviye Python Programlama

---
+++ Turler ve Degiskenler/aylik_yol_masrafi_app.py
@@ -0,0 +1,12 @@
+#örnek1
+# gidis_masrafi=3.65
+# donus_masrafi=4.20
+# aylik_masraf=gidis_masrafi+donus_masrafi*30
+# print(aylik_masraf)
+gidis=input("Günlük İşe Gidiş Ücretinizi Giriniz:")
+gidis=float(gidis)
+donus=input("Günlük İşten Dönüş Ücretinizi Giriniz:")
+donus=float(donus)
+aylik_yol_masrafi=(gidis+donus)*30
+print("Aylik Yol Masrafınız :",aylik_yol_masrafi)
+
